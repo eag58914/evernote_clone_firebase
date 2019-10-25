1af400508f95ae3e3c2e471ac0f5b520a7fc8ee9
Elco Garcia <garciaelco18@gmail.com>
2019-10-25 13:58:11 -0500

    recodified into class function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const firebase = require('firebase');
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			selectedNoteIndex: null,
+			selectedNote: null,
+			notes: null
+		};
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	render() {
+		return <h1>Hello World</h1>;
+	}
+	componentDidMount = () => {
+		firebase.firestore().collection('notes').onSnapshot((serverUpdate) => {
+			const notes = serverUpdate.docs.map((_doc) => {
+				const data = _doc.data();
+				data['id'] = _doc.id;
+				return data;
+			});
+			console.log(notes);
+			this.setState({ notes: notes });
+		});
+	};
 }
 
 export default App;
