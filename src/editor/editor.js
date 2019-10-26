02bfe78045bad1bc5148f32e0dcbc266385a6fc7
Elco Garcia <garciaelco18@gmail.com>
2019-10-26 18:10:49 -0500

    imported necessar files and created classes

---
+++ src/editor/editor.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class EditorComponent extends Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return <div>Hello from the author</div>;
+	}
+}
+
+export default withStyles(styles)(EditorComponent);
