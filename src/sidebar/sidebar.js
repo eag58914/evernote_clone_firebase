198b114ee863edab57c7f88661850a055d8d396a
Elco Garcia <garciaelco18@gmail.com>
2019-10-29 17:26:58 -0500

    updated to have func

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -64,7 +64,8 @@ class SideBarComponent extends Component {
 		this.setState({ title: txt });
 	};
 	newNote = () => {
-		console.log(this.state);
+		this.props.newNote(this.state.title);
+		this.setState({ title: null, addingNote: false });
 	};
 	selectNote = (n, i) => {
 		this.props.selectNote(n, i);
